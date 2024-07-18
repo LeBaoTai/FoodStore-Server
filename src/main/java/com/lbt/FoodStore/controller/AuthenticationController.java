f390d8190761a63a87c33e0a80919820ef22a47b
FiaT <fnight644@gmail.com>
2024-07-18 23:41:23 +0700

    fix: fix: configure cors filter for auth API

--- src/main/java/com/lbt/FoodStore/controller/AuthenticationController.java
+++ src/main/java/com/lbt/FoodStore/controller/AuthenticationController.java
@@ -13,10 +13,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
@@ -24,6 +21,7 @@ import java.text.ParseException;
 @RequestMapping("/auth")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Builder
+@CrossOrigin
 public class AuthenticationController {
     @Autowired
     AuthenticationService service;
